ba46288dc4b9419925ab91f6119890d9ed0d3680
Yuxi <yuxi.25@intl.zju.edu.cn>
2026-03-11 13:58:38 +0800

    Add the booleans

---
+++ Practical4/booleans.py
@@ -0,0 +1,12 @@
+X=True
+Y=False
+W=X or Y
+print("X:", X)
+print("Y:", Y)
+print("W (X or Y):", W)
+# Truth table for W = X or Y
+# X      Y      W
+# True   True   True
+# True   False  True
+# False  True   True
+# False  False  False
